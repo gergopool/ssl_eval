0b58d8f682f29ab05c6df944c355a028a83ed640
Gergely Papp <gergely.papp@renyi.hu>
2022-02-07 19:47:59 +0000

    shortened version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 
 setup(
     name='ssl_eval',
-    version='0.0.0.1',
+    version='0.0.1',
     author='Gergely Papp',
     author_email='gergopool@gmail.com',
     packages=find_packages(),
