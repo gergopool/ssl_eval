45829a921605d6677a4c0012777d2ef8e5d86865
Gergely Papp <gergely.papp@renyi.hu>
2022-02-09 13:55:27 +0000

    representations moved to ram

--- ssl_eval/eval.py
+++ ssl_eval/eval.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn.functional as F
+import torchvision
 from torch import nn
 from typing import Union, Tuple
 import pkbar
@@ -30,19 +31,24 @@ class Evaluator:
         data_loaders = get_loaders_by_name(root, dataset, batch_size=batch_size)
         self.train_loader, self.val_loader = data_loaders
 
-    def generate_embeddings(self) -> Tuple[torch.Tensor, torch.Tensor]:
+    def generate_embeddings(self, flip=False) -> Tuple[torch.Tensor, torch.Tensor]:
 
         train_z, train_y = [], []
 
         if self.verbose:
-            pbar = pkbar.Pbar(name='Generating embeddings', target=len(self.train_loader))
+            pbar = pkbar.Pbar(name='\nGenerating embeddings', target=len(self.train_loader))
 
         with torch.cuda.amp.autocast(enabled=True):
             with torch.no_grad():
                 for i, (x, y) in enumerate(self.train_loader):
                     x = x.cuda()
-                    y = y.cuda()
+                    if flip:
+                        x = torch.cat((x, torchvision.transforms.functional.hflip(x)), dim=0)
+                        y = torch.cat((y, y), dim=0)
+
                     z = self.model(x)
+                    z = AllGather.apply(z).cpu()
+                    y = AllGather.apply(y).cpu()
 
                     train_z.append(z)
                     train_y.append(y)
@@ -50,10 +56,20 @@ class Evaluator:
                     if self.verbose:
                         pbar.update(i)
 
-                train_z = AllGather.apply(torch.cat(train_z))
-                train_y = AllGather.apply(torch.cat(train_y))
+                train_z = torch.cat(train_z)
+                train_y = torch.cat(train_y)
 
-        return train_z, train_y
+        return train_z.float(), train_y
+
+    def _get_start_weights(self, train_z, train_y):
+        n_classes = int(train_y.max() + 1)
+        z = F.normalize(train_z, dim=1).float()
+        weights = torch.zeros(n_classes, train_z.shape[-1]).cuda()
+        for y in range(n_classes):
+            i = (train_y == y).nonzero().ravel()
+            mean_vec = z[i].mean(dim=0)
+            weights[y] = F.normalize(mean_vec, dim=0) * 5
+        return weights
 
     def linear_eval(self,
                     train_z: torch.Tensor,
@@ -63,26 +79,32 @@ class Evaluator:
                     lr: float = 1e-2) -> torch.Tensor:
 
         # Define linear classifier
-        classifier = nn.Linear(self.cnn_dim, train_y.max() + 1).cuda()
-        classifier.weight.data.normal_(mean=0.0, std=0.01)
+        n_classes = int(train_y.max() + 1)
+        classifier = nn.Linear(self.cnn_dim, n_classes).cuda()
+        classifier.weight.data.copy_(self._get_start_weights(train_z, train_y))
         classifier.bias.data.zero_()
         if self.world_size > 1:
             classifier = nn.parallel.DistributedDataParallel(classifier)
 
         # Optimizer and loss
         opt = torch.optim.SGD(classifier.parameters(), lr=lr, momentum=0.9)
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(opt, epochs)
         criterion = nn.CrossEntropyLoss()
 
         # Choose large batch size for quick evaluation
         if batch_size is None:
-            batch_size = 16384 if self.dataset == 'imagenet' else 512
+            batch_size = 4096 if self.dataset == 'imagenet' else 256
+            batch_size = int(batch_size / self.world_size)
+
+        lr = lr * batch_size * self.world_size / 256
 
         # Iterations per epoch
         ipe = len(train_z) // batch_size
 
         if self.verbose:
+            print(f"\nLinear Eval - params: lr={lr:0.5f} | batch_size={batch_size}")
             # Progress bar
-            pbar = pkbar.Pbar(f"\nLinear Eval - Training {epochs} epochs.", target=epochs)
+            pbar = pkbar.Pbar(f"Linear Eval - Training {epochs} epochs.", target=epochs)
 
         # Train N epochs
         for epoch in range(epochs):
@@ -90,19 +112,21 @@ class Evaluator:
             indices = torch.randperm(len(train_z))
 
             for i in range(ipe):
+                opt.zero_grad()
                 # Prepare data
                 j = indices[i * batch_size:(i + 1) + batch_size]
-                z = train_z[j].float()  # Ensure using float32 for high precision
-                y = train_y[j]
+                z = train_z[j].cuda().float()  # Ensure using float32 for high precision
+                y = train_y[j].cuda()
 
                 # Evaluate
                 y_hat = classifier(z)
                 loss = criterion(y_hat, y)
 
                 # Backprop
-                opt.zero_grad()
                 loss.backward()
                 opt.step()
+
+            scheduler.step()
 
             if self.verbose:
                 pbar.update(epoch)
@@ -112,7 +136,8 @@ class Evaluator:
             pbar = pkbar.Kbar(target=len(self.val_loader))
 
         # Evaluate once
-        accs = []
+        hits = torch.zeros(1).cuda()
+        total = 0
         for x, y in self.val_loader:
             # Prepare input x and y
             x = x.cuda()
@@ -120,24 +145,28 @@ class Evaluator:
 
             # Predict
             with torch.no_grad():
-                y_hat = classifier(self.model(x))
+                with torch.cuda.amp.autocast(enabled=True):
+                    z = self.model(x)
+                y_hat = classifier(z.float())
 
             # Get accuracy
-            acc = accuracy(y_hat, y)
-            accs.append(acc)
+            batch_hits = (y_hat.argmax(1) == y).sum()
+            hits += batch_hits
+            total += len(y)
 
             if self.verbose:
-                pbar.add(1, values=[("acc", acc)])
+                pbar.add(1, values=[("acc", batch_hits / len(y))])
 
         # Mean accuracy over all ranks
-        acc = AllReduce.apply(torch.Tensor(accs).cuda()).mean()
+        acc = AllReduce.apply(hits / total)[0]
 
         if self.verbose:
-            print(f"Top1 @ Linear Eval: {acc*100:3.2f}%")
+            acc_value = acc.cpu().numpy()
+            print(f"Top1 @ Linear Eval: {acc_value*100:3.2f}%")
 
         return acc
 
-    def _knn(self, train_z: torch.Tensor, train_y: torch.Tensor, ks: list) -> torch.Tensor:
+    def knn(self, train_z: torch.Tensor, train_y: torch.Tensor, ks: list) -> torch.Tensor:
 
         if isinstance(ks, int):
             ks = [ks]
@@ -158,7 +187,7 @@ class Evaluator:
         train_y = train_y.repeat(self.batch_size).view(self.batch_size, -1)  # train labels
 
         if self.verbose:
-            print("KNN-evaluation")
+            print("\nKNN-evaluation")
             pbar = pkbar.Kbar(target=len(self.val_loader))
 
         # Get knn prediction for each batch
@@ -167,11 +196,11 @@ class Evaluator:
                 with torch.no_grad():
                     # Current batch's embeddings and labels
                     x = x.cuda()
-                    y = y.cuda()
-                    z = F.normalize(self.model(x))
+                    y = y.cpu()
+                    z = F.normalize(self.model(x)).cpu()
 
                 # This batch's accuracy (for logging purpose)
-                batch_hits = torch.zeros(len(ks)).cuda()
+                batch_hits = torch.zeros(len(ks))
 
                 # Distance matrix
                 dist = z @ train_z.T
@@ -203,174 +232,3 @@ class Evaluator:
                 print(f"Top1 @ K={k:<2d} : {acc*100:3.2f}%")
 
         return accuracies
-
-
-class KNNEvaluator:
-
-    def __init__(self,
-                 model: nn.Module,
-                 cnn_dim: int,
-                 dataset: str,
-                 root: str,
-                 batch_size=256,
-                 verbose=True):
-        self.model = model
-        self.cnn_dim = cnn_dim
-        self.dataset = dataset
-        self.batch_size = batch_size
-        self.verbose = verbose and get_world_size_n_rank()[1] == 0
-
-        data_loaders = get_loaders_by_name(root, dataset, batch_size=batch_size)
-        self.train_loader, self.val_loader = data_loaders
-
-    def _get_labeled_embeddings(self):
-
-        features, labels = [], []
-
-        if self.verbose:
-            pbar = pkbar.Pbar(name='Generating embeddings', target=len(self.train_loader))
-
-        for i, (x, y) in enumerate(self.train_loader):
-            x = x.cuda()
-            y = y.cuda()
-            z = self.model(x)
-
-            features.append(z)
-            labels.append(y)
-
-            if self.verbose:
-                pbar.update(i)
-
-        features = AllGather.apply(torch.cat(features))
-        labels = AllGather.apply(torch.cat(labels))
-
-        return features, labels
-
-    def _lin_eval(self,
-                  train_z: torch.Tensor,
-                  train_y: torch.Tensor,
-                  epochs: int = 100) -> torch.Tensor:
-        classifier = nn.Linear(self.cnn_dim, train_y.max() + 1).cuda()
-        classifier.weight.data.normal_(mean=0.0, std=0.01)
-        classifier.bias.data.zero_()
-
-        if get_world_size_n_rank()[0] > 1:
-            classifier = nn.parallel.DistributedDataParallel(classifier)
-
-        opt = torch.optim.SGD(classifier.parameters(), lr=1e-2, momentum=0.9)
-        criterion = nn.CrossEntropyLoss()
-
-        batch_size = 16384 if self.dataset == 'imagenet' else 512
-        ipe = len(train_z) // batch_size
-
-        if self.verbose:
-            pbar = pkbar.Pbar(f"Linear Eval - Training {epochs} epochs.", target=epochs * ipe)
-
-        for epoch in range(epochs):
-            indices = torch.randperm(len(train_z))
-            for i in range(ipe):
-                start = i * batch_size
-                end = start + batch_size
-                j = indices[start:end]
-                x = train_z[j].float()
-                y = train_y[j]
-
-                y_hat = classifier(x)
-                loss = criterion(y_hat, y)
-                opt.zero_grad()
-                loss.backward()
-                opt.step()
-
-                if self.verbose:
-                    pbar.update(epoch * ipe + i)
-
-        if self.verbose:
-            print(f"Linear Eval - Evaluating.")
-            pbar = pkbar.Kbar(target=len(self.val_loader))
-
-        accs = []
-        for x, y in self.val_loader:
-            x = x.cuda()
-            y = y.cuda()
-            with torch.no_grad():
-                y_hat = classifier(self.model(x))
-            acc = accuracy(y_hat, y)
-            accs.append(acc)
-            if self.verbose:
-                pbar.add(1, values=[("acc", acc)])
-
-        acc = AllReduce.apply(torch.Tensor(accs).cuda()).mean()
-
-        if self.verbose:
-            print(f"Top1 @ Linear Eval: {acc*100:3.2f}%")
-
-        return acc
-
-    def _evaluate(self, train_z: torch.Tensor, train_y: torch.Tensor, ks: list) -> torch.Tensor:
-
-        train_z = F.normalize(train_z, dim=1)
-        ks = [int(k) for k in ks]
-        largest_k = max(ks)
-        results = torch.zeros(len(ks)).cuda()
-        train_y = train_y.repeat(self.batch_size).view(self.batch_size, -1)
-
-        if self.verbose:
-            print("KNN-evaluation")
-            pbar = pkbar.Kbar(target=len(self.val_loader))
-
-        total = 0
-        for x, y in self.val_loader:
-            x = x.cuda()
-            y = y.cuda()
-            z = F.normalize(self.model(x))
-
-            batch_results = torch.zeros(len(ks)).cuda()
-
-            dist = z @ train_z.T
-            closest_indices = dist.topk(largest_k, dim=1)[1]
-            pred_labels = torch.gather(train_y, dim=1, index=closest_indices)
-            for j, k in enumerate(ks):
-                preds = pred_labels[:, :k].mode(dim=1)[0]
-                hits = (preds == y).sum()
-                batch_results[j] += hits
-
-            results += batch_results
-            total += len(y)
-
-            if self.verbose:
-                accs = list(batch_results.cpu().numpy() * 100 / len(y))
-                update_values = [(f"k={k}", acc) for (k, acc) in zip(ks, accs)]
-                pbar.add(1, values=update_values)
-
-        results /= total
-        results = AllReduce.apply(results)
-
-        if self.verbose:
-            accs = list(results.cpu().numpy())
-            for k, acc in zip(ks, accs):
-                print(f"Top1 @ K={k:<2d} : {acc*100:3.2f}%")
-
-        return results
-
-    def __call__(self, ks: Union[int, list] = 1) -> torch.Tensor:
-
-        if isinstance(ks, int):
-            ks = [ks]
-
-        if not isinstance(ks, list):
-            raise TypeError(f"Value k must be either a list or int, not {type(ks)}")
-
-        # Freee batchnorm
-        training_mode_used_before = self.model.training
-        self.model.eval()
-
-        with torch.cuda.amp.autocast(enabled=True):
-            with torch.no_grad():
-                train_features, train_labels = self._get_labeled_embeddings()
-                results = self._evaluate(train_features, train_labels, ks)
-
-        self._lin_eval(train_features, train_labels)
-
-        self.model.train(training_mode_used_before)
-
-        return results
---
+++ test.py
@@ -0,0 +1,208 @@
+# From: https://github.com/kuangliu/pytorch-cifar/blob/master/main.py
+'''Train CIFAR10 with PyTorch.'''
+import os
+
+os.environ['CUDA_VISIBLE_DEVICES'] = "7"
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+import torchvision
+import torchvision.transforms as transforms
+
+import argparse
+
+from ssl_eval.eval import Evaluator
+
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes,
+                               planes,
+                               kernel_size=3,
+                               stride=stride,
+                               padding=1,
+                               bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes,
+                          self.expansion * planes,
+                          kernel_size=1,
+                          stride=stride,
+                          bias=False),
+                nn.BatchNorm2d(self.expansion * planes))
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion * planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion * planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion * planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes,
+                          self.expansion * planes,
+                          kernel_size=1,
+                          stride=stride,
+                          bias=False),
+                nn.BatchNorm2d(self.expansion * planes))
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        return out
+
+
+class ResNet18(nn.Module):
+
+    def __init__(self, num_classes=10):
+        super(ResNet18, self).__init__()
+        self.encoder = ResNet(BasicBlock, [2, 2, 2, 2])  # resnet18
+        self.prev_dim = self.encoder.fc.weight.shape[1]
+        self.encoder.fc = nn.Identity()
+        self.classifier = nn.Linear(self.prev_dim, num_classes)
+
+    def forward(self, x):
+        z = self.encoder(x)
+        return self.classifier(z)
+
+
+parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
+args = parser.parse_args()
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+best_acc = 0  # best test accuracy
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+
+# Data
+print('==> Preparing data..')
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+trainset = torchvision.datasets.CIFAR10(root='/data/shared/data/cifar10',
+                                        train=True,
+                                        download=True,
+                                        transform=transform_train)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=8)
+
+testset = torchvision.datasets.CIFAR10(root='/data/shared/data/cifar10',
+                                       train=False,
+                                       download=True,
+                                       transform=transform_test)
+testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=8)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+# Model
+print('==> Building model..')
+net = ResNet18()
+net = net.to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
+
+
+# Training
+def train(epoch):
+    print('\nEpoch: %d' % epoch)
+    net.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+    for inputs, targets in trainloader:
+        inputs, targets = inputs.to(device), targets.to(device)
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+
+
+def test(epoch):
+    evaluator = Evaluator(net.encoder, 512, "cifar10", "/data/shared/data/cifar10")
+    train_z, train_y = evaluator.generate_embeddings(flip=True)
+    evaluator.linear_eval(train_z, train_y)
+
+
+for epoch in range(2):
+    train(epoch)
+
+test(epoch)
