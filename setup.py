833749d01f52ba81e250f283343f682f30657436
Gergely Papp <gergopool@gmail.com>
2022-05-03 10:38:36 +0000

    0.1.6

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md') as f:
 
 setup(
     name='ssl_eval',
-    version='0.1.5',
+    version='0.1.6',
     author='Gergely Papp',
     author_email='gergopool@gmail.com',
     packages=find_packages(),
